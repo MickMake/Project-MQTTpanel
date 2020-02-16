5ae4ac2ba97f825c9a2edaee4bcb2dcc929dafd5
Timmah88 <59159135+Timmah88@users.noreply.github.com>
2020-02-16 14:38:32 +0100

    Update create.sh
    timmah88

--- create.sh
+++ create.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker create --name project-mqttpanel --restart unless-stopped -p 1883:1883 --device /dev/mem:/dev/mem --privileged --user root mickmake/project-mqttpanel:latest
+docker create --name project-mqttpanel --restart unless-stopped -p 1883:1883 --device /dev/mem:/dev/mem --privileged --user root timmah88/project-mqttpanel:latest
 docker start project-mqttpanel
