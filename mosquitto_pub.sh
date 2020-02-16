01faf7bac9eb317ac8f6b98f7fa712a7c7871af1
Timmah88 <59159135+Timmah88@users.noreply.github.com>
2020-02-16 14:46:13 +0100

    Update mosquitto_pub.sh
    timmah88

--- mosquitto_pub.sh
+++ mosquitto_pub.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec docker run --rm -i -t mickmake/project-mqttpanel:latest /usr/bin/mosquitto_pub -d -h 172.17.0.1 "$@"
+exec docker run --rm -i -t timmah88/project-mqttpanel:latest /usr/bin/mosquitto_pub -d -h 172.17.0.1 "$@"
